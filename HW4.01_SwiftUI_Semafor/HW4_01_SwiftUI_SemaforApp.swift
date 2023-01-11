2cc0f97c2bd52770a1f0bea1d4e817ecf7447104
Sergey Sokolov <sergey.and.sokolov@gmail.com>
2023-01-11 23:00:05 +0400

    Initial Commit

---
+++ HW4.01_SwiftUI_Semafor/HW4_01_SwiftUI_SemaforApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW4_01_SwiftUI_SemaforApp.swift
+//  HW4.01_SwiftUI_Semafor
+//
+//  Created by MacBook Air 13 on 11.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW4_01_SwiftUI_SemaforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
